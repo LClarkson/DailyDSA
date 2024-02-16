cd0a561dac58f8fea5f53745c857ef717c67d546
LClarkson <l.j.clarkson@gmail.com>
2024-02-16 11:54:38 -0800

    leet 11

--- leet-11-container-with-most-water.js
+++ leet-11-container-with-most-water.js
@@ -8,4 +8,54 @@ such that the container contains the most water.
 Return the maximum amount of water a container can store.
 
 Notice that you may not slant the container.
+
+Example 1:
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
+In this case, the max area of water (blue section) the container can contain is 49.
+
+Example 2:
+Input: height = [1,1]
+Output: 1
+
+
 */
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const maxArea = function(height) {
+
+    // create pointers to traverse array
+    let left = 0;
+    let right = height.length-1;
+    let max = 0;
+
+   
+    while (left < right) {
+        let currentContainerHeight;
+        let area;
+        let currentContainerWidth = right - left;
+
+        if (height[left] < height[right]) {
+            currentContainerHeight = height[left];
+            left++;
+        } else {
+            currentContainerHeight = height[right];
+            right--;
+        }
+
+        area = currentContainerWidth * currentContainerHeight
+
+        if (area > max) max = area;
+        
+    }
+
+
+   return max;
+   
+};
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]));
